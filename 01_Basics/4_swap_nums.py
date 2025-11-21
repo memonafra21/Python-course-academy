37b4c491567ed08eef173e4562a4f693291d116d
memonafra21 <memonafra123@gmail.com>
2025-11-21 12:15:10 +0530

    First Comment

--- 01_Basics/4_swap_nums.py
+++ 01_Basics/4_swap_nums.py
@@ -1,7 +1,21 @@
+
 # Take two numbers as input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Print before swapping
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
 
 # Store one value in temp
+temp = a
 
 # Swap the variables
+a = b
+b = temp
 
-# Print before and after swapping
+# Print after swapping
+print("After swapping:")
+print("a =", a)
+print("b =", b)
--- 04_Conditionals/3_calc_grade.py
+++ 04_Conditionals/3_calc_grade.py
@@ -1,3 +1,21 @@
-# Input marks from 0 to 100
-# Use conditions to assign grade
-# Print the grade
+
+
+marks = int(input("enter your marks: "))
+grade_range = marks // 10
+grade = ""
+match grade_range:
+    case 10 | 9:
+        grade = "A"
+    case 8: 
+        grade = "B"
+    case 7:
+        grade = "C"
+    case 6: 
+        grade = "D"
+    case _:
+        grade = "F"
+
+if grade == "F":
+    print("you have failed the exam")
+else:
+    print(f"your grade is {grade}")
