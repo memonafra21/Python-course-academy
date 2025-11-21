ea4bc641bfa12fd90740dacd8e4097e19b81649c
memonafra21 <memonafra123@gmail.com>
2025-11-21 20:22:26 +0530

    updated files

--- 04_Conditionals/4_leap_year.py
+++ 04_Conditionals/4_leap_year.py
@@ -1,3 +1,9 @@
 # Input a year
 # Check if divisible by 4
 # Print if leap year or not
+year = int(input("Enter a year: "))
+
+if year % 4 == 0:
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
