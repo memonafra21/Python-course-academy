edda28a700300e63468c1cf7b13923f0ee9ed238
memonafra21 <memonafra123@gmail.com>
2025-11-25 11:55:39 +0530

    updated files

--- 02_Data_and_operator/2_simple_interest.py
+++ 02_Data_and_operator/2_simple_interest.py
@@ -1,5 +1,10 @@
 # Input principal, rate, and time
+principal = int(input("Enter principal amount: "))
+rate = int(input("Enter rate of interest: "))
+time = int(input("Enter time in years: "))
 
 # Calculate simple interest
+interest = (principal * rate * time) / 100
 
 # Print the interest
+print("Simple Interest =", interest)
