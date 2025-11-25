72544fb3f45927f6f4e214ab7c567f41cd3c07e2
memonafra21 <memonafra123@gmail.com>
2025-11-25 12:00:38 +0530

    updated files

--- 02_Data_and_operator/3_permiter_of_square.py
+++ 02_Data_and_operator/3_permiter_of_square.py
@@ -1,3 +1,8 @@
 # Input side length
+side = int(input("Enter side length: "))
+
 # Calculate perimeter (4 * side)
+perimeter = 4 * side
+
 # Print the perimeter
+print("Perimeter of square =", perimeter)
