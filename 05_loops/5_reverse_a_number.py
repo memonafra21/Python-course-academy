f884518207752fcfeb23a14cc27905d2ee2b1683
memonafra21 <memonafra123@gmail.com>
2025-11-27 16:40:03 +0530

    updated files

--- 05_loops/5_reverse_a_number.py
+++ 05_loops/5_reverse_a_number.py
@@ -1,3 +1,13 @@
 # Input a number
-# Use loop to reverse the digits of the number
+num = int(input("Enter a number: "))
+
+rev = 0   # to store reversed number
+
+# Use loop to reverse the digits
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //= 10
+
 # Print the reversed number
+print("Reversed number:", rev)
