8f3ce4c6201f9a7bf4dd81991c7091763a54f05d
memonafra21 <memonafra123@gmail.com>
2025-11-27 12:57:50 +0530

    updated files

--- 05_loops/1_print_natural.py
+++ 05_loops/1_print_natural.py
@@ -1,3 +1,14 @@
 # Input a number n
-# Use a loop to print numbers from 1 to n
-# Use another loop to print numbers from n to 1 (reverse order)
+n = int(input("Enter a number: "))
+
+# Loop to print numbers from 1 to n
+print("Numbers from 1 to", n, ":")
+for i in range(1, n + 1):
+    print(i, end=" ")
+
+print()  # for new line
+
+# Loop to print numbers from n to 1 (reverse order)
+print("Numbers from", n, "to 1:")
+for i in range(n, 0, -1):
+    print(i, end=" ")
