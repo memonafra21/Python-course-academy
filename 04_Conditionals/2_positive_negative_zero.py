914833fe58f1966aa8aa3efddc0fe717b4afd113
memonafra21 <memonafra123@gmail.com>
2025-11-27 16:33:46 +0530

    updated files

--- 04_Conditionals/2_positive_negative_zero.py
+++ 04_Conditionals/2_positive_negative_zero.py
@@ -1,3 +1,10 @@
 # Input a number
+num = float(input("Enter a number: "))
+
 # Check if positive, negative, or zero
-# Print the result
+if num > 0:
+    print("The number is Positive")
+elif num < 0:
+    print("The number is Negative")
+else:
+    print("The number is Zero")
