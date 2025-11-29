bcfcf630392622879b070b674ddfaf02b0b013f3
memonafra21 <memonafra123@gmail.com>
2025-11-29 20:04:18 +0530

    updated files

--- 05_loops/4_sum_of_digits.py
+++ 05_loops/4_sum_of_digits.py
@@ -1,5 +1,19 @@
 # Input a number
-# Use loop to extract digits (using % and //)
-# Maintain sum of digits
-# Maintain product of digits
-# Print both values
+num = int(input("Enter a number: "))
+
+# Store original number for display (optional)
+temp = num
+
+sum_digits = 0
+product_digits = 1
+
+# Use loop to extract digits
+while num > 0:
+    digit = num % 10       # extract last digit
+    sum_digits += digit    # add to sum
+    product_digits *= digit  # multiply for product
+    num = num // 10        # remove last digit
+
+# Print values
+print("Sum of digits =", sum_digits)
+print("Product of digits =", product_digits)
