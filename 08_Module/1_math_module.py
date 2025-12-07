60ed3c68afb05050621da83ddd17220ab3049be1
memonafra21 <memonafra123@gmail.com>
2025-12-07 14:07:10 +0530

    updated files

---
+++ 08_Module/1_math_module.py
@@ -0,0 +1,14 @@
+import math
+
+# Input as integer
+num = int(input("Enter an integer: "))
+
+# Calculations using math module
+square = math.pow(num, 2)
+cube = math.pow(num, 3)
+square_root = math.sqrt(num)
+
+# Output
+print("Square:", square)
+print("Cube:", cube)
+print("Square Root:", square_root)
